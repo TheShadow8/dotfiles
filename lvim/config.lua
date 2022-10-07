69c20923889bf4493c649e6c8328884aeed6e90f
Minh Do <35181893+TheShadow8@users.noreply.github.com>
2022-10-07 16:22:28 +0700

    Update config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -21,19 +21,20 @@ keymap("n", "<C-s>", ":w<CR>", options)
 keymap("i", "<C-s>", "<ESC>:w<CR>", options)
 keymap("v", "<C-s>", "<ESC>:w<CR>", options)
 
+keymap("t", "<esc>", [[<C-\><C-n>]], options)
+keymap("t", "jk", [[<C-\><C-n>]], options)
+keymap("t", "<C-h>", [[<Cmd>wincmd h<CR>]], options)
+keymap("t", "<C-j>", [[<Cmd>wincmd j<CR>]], options)
+keymap("t", "<C-k>", [[<Cmd>wincmd k<CR>]], options)
+keymap("t", "<C-l>", [[<Cmd>wincmd l<CR>]], options)
+
 -- Map commentary
 keymap("v", "<C-_>", "gc", {})
 keymap("n", "<C-_>", "gcc", {})
 keymap("i", "<C-_>", "<Esc>VgcA", {})
 
-keymap(
-  "n",
-  "<C-p>",
-  '<cmd>lua require("telescope.builtin").find_files({ hidden=true, no_ignore=true })<CR>',
-  options
-)
+keymap("n", "<C-p>", '<cmd>lua require("telescope.builtin").find_files({ hidden=true, no_ignore=true })<CR>', options)
 
--- unmap a default keymapping
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
@@ -44,13 +45,14 @@ lvim.builtin.telescope.defaults = {
       prompt_position = "top"
     }
   },
-  file_ignore_patterns = { "node_modules", "^.git/" },
+  file_ignore_patterns = { "node_modules", "^.git/" }
 }
 
-lvim.builtin.terminal.direction = "horizontal"
+lvim.builtin.terminal.direction = "vertical"
 lvim.builtin.terminal.open_mapping = "<F12>"
+lvim.builtin.terminal.size = 25
 -- Use which-key to add extra bindings with the leader-key prefix
-lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope grep_string <CR>", "Find text" }
+lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope live_grep <CR>", "Find text" }
 lvim.builtin.which_key.mappings["F"] = { "<cmd>Telescope grep_string <CR>", "Find current word" }
 lvim.builtin.which_key.mappings["p"] = { require("lvim.core.telescope.custom-finders").find_project_files, "Find File" }
 lvim.builtin.which_key.mappings["P"] = {
@@ -60,17 +62,20 @@ lvim.builtin.which_key.mappings["P"] = {
   r = { "<cmd>lua require('lvim.plugin-loader').recompile()<cr>", "Re-compile" },
   s = { "<cmd>PackerSync<cr>", "Sync" },
   S = { "<cmd>PackerStatus<cr>", "Status" },
-  u = { "<cmd>PackerUpdate<cr>", "Update" },
-
+  u = { "<cmd>PackerUpdate<cr>", "Update" }
 }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "startify"
+
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+-- lvim.builtin.alpha.active = false
+--
+lvim.builtin.project.manual_mode = true
+lvim.builtin.nvimtree.setup.sync_root_with_cwd = false
+lvim.builtin.nvimtree.setup.update_cwd = false
+lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -86,7 +91,7 @@ lvim.builtin.treesitter.ensure_installed = {
   "css",
   "rust",
   "java",
-  "yaml",
+  "yaml"
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -96,12 +101,140 @@ lvim.builtin.treesitter.highlight.enabled = true
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "gofumpt", arg = "-extra", filetypes = { "go" } },
-  { command = "goimports", filetypes = { "go" } },
+  { command = "goimports", filetypes = { "go" } }
 }
 
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { command = "eslint_d", filetypes = { "javascript" } },
+  { command = "eslint_d", filetypes = { "typescript" } }
+}
 
 -- Additional Plugins
 lvim.plugins = {
-  { "ghifarit53/tokyonight-vim" },
+  { "ghifarit53/tokyonight-vim" }
 }
 
+local startify = require("alpha.themes.startify")
+local fortune = require("alpha.fortune")
+
+local logo = {
+  type = "text",
+  val = {
+    [[                                                                              ]],
+    [[                                    ██████                                    ]],
+    [[                                ████▒▒▒▒▒▒████                                ]],
+    [[                              ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                              ]],
+    [[                            ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                            ]],
+    [[                          ██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒                              ]],
+    [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▓▓▓▓                          ]],
+    [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▒▒▓▓                          ]],
+    [[                        ██▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ██                        ]],
+    [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+    [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+    [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+    [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+    [[                        ██▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒██                        ]],
+    [[                        ████  ██▒▒██  ██▒▒▒▒██  ██▒▒██                        ]],
+    [[                        ██      ██      ████      ████                        ]],
+    [[                                                                              ]],
+    [[                                                                              ]]
+  },
+  opts = {
+    position = "center",
+    hl = "Statement"
+  }
+}
+
+local function info_value()
+  local datetime = os.date(" %d-%m-%Y")
+
+  return datetime
+end
+
+local info = {
+  type = "text",
+  val = info_value(),
+  opts = {
+    hl = "DevIconVim",
+    position = "center"
+  }
+}
+
+local message = {
+  type = "text",
+  val = fortune({ max_width = 60 }),
+  opts = {
+    position = "center",
+    hl = "Statement"
+  }
+}
+
+local header = {
+  type = "group",
+  val = {
+    logo,
+    info,
+    message
+  }
+}
+
+local mru = {
+  type = "group",
+  val = {
+    {
+      type = "text",
+      val = "Current Dir",
+      opts = {
+        hl = "String",
+        shrink_margin = false,
+        position = "center"
+      }
+    },
+    { type = "padding", val = 1 },
+    {
+      type = "group",
+      val = function()
+        return { startify.mru(1, vim.fn.getcwd(), 6) }
+      end
+    }
+  }
+}
+
+local mru2 = {
+  type = "group",
+  val = {
+    {
+      type = "text",
+      val = "Recent files",
+      opts = {
+        hl = "String",
+        shrink_margin = false,
+        position = "center"
+      }
+    },
+    {
+      type = "group",
+      val = function()
+        return { startify.mru(7, nil, 4) }
+      end
+    }
+  }
+}
+
+local config = {
+  layout = {
+    header,
+    { type = "padding", val = 1 },
+    mru,
+    { type = "padding", val = 1 },
+    mru2
+  },
+  opts = {
+    margin = 5
+  }
+}
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "startify"
+lvim.builtin.alpha["startify"].config = config
